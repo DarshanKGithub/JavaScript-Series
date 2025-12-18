80c3929dcde2f58726c5468f6c3eb4d104c36745
DarshanKGithub <darshankshetri8@gmail.com>
2025-12-18 20:02:37 +0530

    also added SecondLargest String in String

--- LongestString.js
+++ LongestString.js
@@ -2,15 +2,22 @@ const str = "My Name is Darshan";
 
 function longestString(str){
     let longest = '';
+    let secondLargest = '';
 
     let words = str.split(' ');
     console.log(words);
 
     for(const word of words){
         if(word.length > longest.length){
-            longest = word;        }
+            secondLargest = longest;
+            longest = word;      
+          }
+          else if(word !== longest && word<= secondLargest){
+            secondLargest = word;
+          }
     }
-     return longest;
+    //  return longest;
+    return secondLargest;
 }
 
 console.log(longestString(str));
