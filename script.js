9ead7a9dd145733622559061d41ce433c1b44d8e
DarshanKGithub <darshankshetri8@gmail.com>
2025-12-19 18:32:48 +0530

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+const green = document.querySelector('.green');
+const pink = document.querySelector('.pink');
+const blue = document.querySelector('.blue');
+
+green.addEventListener('click', (e) => {
+    console.log("Green Clicked");
+    
+})
